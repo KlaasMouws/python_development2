2b4c3a22e9b9f5f50687a879c784136dd8d9db33
KlaasMouws <91462774+KlaasMouws@users.noreply.github.com>
2023-05-31 14:35:58 +0200

    Screenshots klaar

--- screenshot.py
+++ screenshot.py
@@ -1,13 +1,19 @@
-import pyautogui
+from datetime import datetime
+from PIL import ImageGrab
+from github import Github
 import schedule
 import time
-from PIL import Image, ImageGrab
 import os
-from datetime import datetime
+
 
 class Screenshot:
     def __init__(self):
         self.dir = "screenshots"
+        self.access_token = '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'
+
+        self.g = Github(self.access_token)
+        self.user = self.g.get_user()
+        self.repo = self.user.get_repo('python_development')
 
     def take_screen(self):
         # Take screen and name it
@@ -18,6 +24,9 @@ class Screenshot:
         # Save the screenshot in the correct directory
         image_path = os.path.join(self.dir, f"{image_name}.png")
         image.save(image_path)
+
+        with open(image_path,"rb") as file:
+            self.repo.create_file(f"{self.dir}/{image_name}.png", f"Added screenshot {image_name}", file.read())
         return image_path
 
     def auto_screen(self, interval=5):
@@ -26,3 +35,6 @@ class Screenshot:
         while True:
             schedule.run_pending()
             time.sleep(1)
+
+screener = Screenshot()
+screener.auto_screen()
